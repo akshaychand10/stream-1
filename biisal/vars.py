2d3437a57e84d731a76bf40cea112cb19ef6957f
akshaychand10 <122161076+akshaychand10@users.noreply.github.com>
2024-09-16 17:15:12 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -20,7 +20,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002222888781'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002222888781'))
-    PORT = int(getenv('PORT', '8080'))
+    PORT = int(getenv('PORT', '9003'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5721673207").split())  
@@ -39,6 +39,7 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
+    FQDN = 'bots.hostingup.icu:9003'
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://akshaychand:akshaychand@cluster0.3gwaqm0.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None)) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
